553954fbcc1f7ee9e80dbac4765e0463274bf3e7
Gabriele Pintus <gabrielegpintus@gmail.com>
2026-01-27 20:13:31 +0100

    Minor change

--- examples/test_linear.cpp
+++ examples/test_linear.cpp
@@ -6,7 +6,7 @@ using namespace lamp;
 int main() {
     // Create a simple 2-layer MLP
     Linear fc1(784, 128);  // Input: 784 (28x28), Hidden: 128
-    Linear fc2(128, 4);   // Hidden: 128, Output: 4 classes
+    Linear fc2(128, 10);   // Hidden: 128, Output: 10 classes
     
     // Create random input (batch_size=32, features=784)
     Tensor x = Tensor::randn({32, 784});
